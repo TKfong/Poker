953136a97f5f1005755b27c88dc3013602d5966c
Tim <everlastgame@gmail.com>
2018-04-30 23:17:38 -0700

    Added Session keys

---
+++ poker client.py
@@ -0,0 +1,122 @@
+# By Timothy Fong
+# CMPE 209 Poker Project
+import socket
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import hashlib
+import os, random, struct, sys
+import Crypto.Random
+from Crypto.Cipher import AES
+import time
+
+#Declare server's attributes
+server = socket.socket()
+host = "10.0.2.15"
+#host = "127.0.1.1"
+port = 7777
+#Connect to server
+server.connect((host, port))
+
+#Tell server that connection is OK for poker game
+server.sendall("Poker: GO")
+
+#Receive public key string from server
+server_string = server.recv(1024)
+
+#Remove extra characters
+server_string = server_string.replace("public_key=", '')
+server_string = server_string.replace("\r\n", '')
+
+#Convert string to key
+server_public_key = RSA.importKey(server_string)
+#print server_public_key.exportKey()
+#Generate Client public/private key pair
+random_generator = Random.new().read
+client_private_key = RSA.generate(1024, random_generator)
+client_public_key = client_private_key.publickey()
+print client_public_key.exportKey()
+print "a"
+#Server's response if too many clients
+server_response = server.recv(1024)
+server_response = server_response.replace("\r\n", '')
+print "server: " + server_response
+if server_response == "Too many clients":
+	print "Fail: server is currently full"
+	print "Now quiting"
+	server.close()
+	sys.exit()
+	print "b"
+#Otherwise confirm username and password
+#elif server_response == "Start":
+#	print "c"
+#	#Receive request for public key
+#	server_response = server.recv(1024)
+#	server_response = server_response.replace("\r\n", '')
+#	if server_response == "Request Public Key":
+#		server.sendall("public_key=" + client_public_key.exportKey() + "\n")
+#	else:
+#		sys.exit()
+#
+#	print "checking"
+
+elif server_response == "Request Public Key":
+	print "c"
+	server.sendall("public_key=" + client_public_key.exportKey() + "\n")
+
+	#Login to obtain public key
+	username = raw_input("Username: ")
+	type(username)
+	password = raw_input("Password: ")
+	type(password)
+	#hash_user = hashlib.md5(username)
+	#hash_pass = hashlib.md5(password)
+	#message = hash_user.hexdigest() + "#" + hash_pass.hexdigest() + "#" + client_public_key
+	message = "encrypted_login=" + username + "~!@#$%^&*()" + password
+	#Encrypt message
+	encrypted = server_public_key.encrypt(message, 32)
+	#Send Login Info to server
+	server.sendall(str(encrypted))
+	#Receive server reply confirming reception of username and password
+	#server_response = server.recv(1024)
+	#server_response = server_response.replace("\r\n", '')
+	
+	#Receive request for session key
+	server_response = server.recv(1024)
+	encrypted = eval(server_response)
+	#Decrypt message
+	decrypted = client_private_key.decrypt(encrypted)
+	print decrypted
+	#Confirm request
+	if "Request Session Key" in decrypted:
+		#Generate AES key and information
+		key = Crypto.Random.OSRNG.posix.new().read(AES.block_size)
+		#print key
+		#Generate initialization vector
+		IV = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+		#print IV
+		mode = AES.MODE_CBC
+		#Encrypt AES key and IV
+		encrypted = server_public_key.encrypt(key + "!@#$%^&*()" + IV, 32)
+		#Send to server
+		server.sendall("encrypted_message="+str(encrypted))
+	else:
+		print "nono"
+		sys.exit()
+	#Confirm reception of key and IV
+	server_response = server.recv(1024)
+	#Decrypt check message
+	decryptor = AES.new(key, mode, IV=IV)
+	plain = decryptor.decrypt(server_response)
+	plain = plain.replace("~", '')
+	print plain
+		#break
+	#Tell server to finish connection
+	#if message == "quit": break
+
+server.sendall("Quit")
+print "d"
+print(server.recv(1024)) 
+#Quit server response
+server.close()
+# By Timothy Fong
+# CMPE 209 Poker Project
---
+++ poker server.py
@@ -0,0 +1,266 @@
+# By Timothy Fong
+# CMPE 209 Poker Project
+import socket
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import os
+import sys
+import threading
+import mysql.connector
+from Crypto.Cipher import AES
+
+encrypt_str = "encrypted_message="
+account_str = "make_account="
+num_clients = 0
+client_public_key = "public_key="
+encrypt_log = "encrypted_login="
+
+#Generate private and public keys
+#Keys for making accounts
+random_generator = Random.new().read
+private_key0 = RSA.generate(1024, random_generator)
+public_key0 = private_key0.publickey()
+#Keys for getting session keys
+random_generator = Random.new().read
+private_key1 = RSA.generate(1024, random_generator)
+public_key1 = private_key1.publickey()
+
+# Thread class
+class ClientThread(threading.Thread):
+	def __init__ (self, c, addr):
+		self.c = c
+		self.addr = addr
+		threading.Thread.__init__(self)
+		print "Starting new thread"
+
+	#might need to implement socket close at end of thread
+	def run(self):
+		#Wait until data is received.
+		data = self.c.recv(1024)
+		#remove new line character
+		data = data.replace("\r\n", '') 
+
+		#Create account on MySQL database
+		make_account(self.c, data)
+			
+		#Poker Client 
+		
+		#Check account exists on MySQL database
+		if data == "Poker: GO":
+			#Get public key for making accounts
+			global public_key1
+			#Send out public key
+			self.c.send("public_key=" + public_key1.exportKey() + "\n")
+			print "Public key sent to account maker."
+			#Update number of clients
+			global num_clients 
+			num_clients = num_clients + 1
+			print "poker go"
+			#Limit number of clients server can handle to 5 
+			if num_clients > 5:
+				self.c.send("Too many clients")
+				print "Fail: too many clients"
+				num_clients = num_clients - 1
+				print "Number of clients: " + str(num_clients)
+				return
+			#Otherwise prepare to receive account data
+			else:
+				#Request client public key
+				#self.c.send("Start")
+				print "Number of clients: " + str(num_clients)
+				#Request client public key
+				self.c.send("Request Public Key")
+				#Receive client public key
+				data = self.c.recv(1024)
+				#Check client key
+				if client_public_key in data:
+					#Remove extra characters
+					data = data.replace("\r\n", '') 
+					data = data.replace(client_public_key, '')
+					#Convert to key
+					client_key = RSA.importKey(data)
+					print client_key.exportKey()
+				else:
+					print "no public key"
+					#return
+					sys.exit()
+				
+				#Wait until username and password data is received.
+				data = self.c.recv(1024)
+				encrypted = eval(data)
+				#Decrypt message
+				decrypted = private_key1.decrypt(encrypted)
+				
+				#Receive encrypted message
+				if encrypt_log in decrypted: 
+					#Remove extra characters
+					decrypted = decrypted.replace("\r\n", '') 
+					decrypted = decrypted.replace(encrypt_log, '')
+					print "Received:\nEncrypted message = "+str(data)
+					print "Decrypted message = " + decrypted
+					data = decrypted.split("~!@#$%^&*()")
+					#Obtain username and password
+					user = data[0]
+					pw = data[1]
+					#Check if username and password exist on database
+					cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='poker')
+					cursor = cnx.cursor(buffered=True)
+					check_stmt = ("SELECT * FROM users WHERE username = %s and password = %s")
+					cursor.execute(check_stmt, (user, pw, ))				
+					cnx.commit()
+					#Check for a row on the database
+					row_count = cursor.rowcount
+					print("number of affected rows: {}".format(row_count))
+					#If row_count > 0 then username/password on databse
+					if row_count == 0:
+						print "It Does Not Exist"
+						#return
+						sys.exit()
+					#Only one copy of the account on the database
+					elif row_count == 1:
+						print "loki dies"
+						#Request for Session Key
+						message = "Request Session Key"
+						encrypted = client_key.encrypt(message, 32)
+						self.c.send(str(encrypted))
+						print "Getting session key"
+						data = self.c.recv(1024)
+						#remove new line character
+						data = data.replace("\r\n", '') 
+						data = data.replace(encrypt_str, '')
+						encrypted = eval(data)
+						decrypted = private_key1.decrypt(encrypted)
+						data = decrypted.split("!@#$%^&*()")
+						key = data[0]
+						IV = data[1]
+						#self.c.send("Server: OK")
+						print "key: " + key
+						print "IV: " + IV
+						encryptor = AES.new(key, AES.MODE_CBC, IV)
+						text = 'loki dies'
+						if len(text) % 16 != 0:
+							text += '~' * (16 - len(text) % 16)
+						ciphertext = encryptor.encrypt(text)
+						self.c.send(ciphertext)
+
+						#update_stmt = "UPDATE users SET public_key = %s where username = %s and password = %s"
+						#cursor.execute(update_stmt, (data, user, pw, ))
+						#cnx.commit()
+						#print "checking"
+					#Beware if row_count != 1
+					else:
+						print "Multiple copies of account exist"
+						#return
+						sys.exit()
+					cursor.close()
+					cnx.close()
+					#break
+				#while True:
+				#elif decrypted == "Quit": 
+					self.c.send("Thread closed\n")
+					print "Thread closed"
+					num_clients = num_clients - 1
+					print "Number of clients: "+str(num_clients)
+					if num_clients <= 0: return
+
+def Main():
+	#Declaration of server attributes
+	mysocket = socket.socket()
+	host = socket.gethostbyname(socket.getfqdn())
+	port = 7777
+	#host check
+	if host == "127.0.1.1":
+		import commands
+		host = commands.getoutput("hostname -I")
+	print "host = " + host
+
+	#Prevent socket.error: [Errno 98] Address already in use
+	mysocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+	mysocket.bind((host, port))
+	#Socket listening on port
+	mysocket.listen(5)
+
+	while True:
+		#establish connection with client
+		c, addr = mysocket.accept()
+		#start a new thread and return its identifier
+		ClientThread(c, addr).start()
+
+	#Server to stop
+	c.send("Server stopped\n")
+	print "Server stopped"
+	c.close()
+	#cursor.close()
+	#cnx.close()
+
+def make_account(c, data):
+	#Wait until data is received.
+	#data = c.recv(1024)
+	#data = data.replace("\r\n", '') #remove new line character
+	
+	#Create account on MySQL database
+	if data == "Account: OK":
+		#Get public key for making accounts
+		global public_key0
+		#Send out public key
+		c.send("public_key=" + public_key0.exportKey() + "\n")
+		print "Public key sent to account maker."
+		#Update number of clients
+		global num_clients 
+		num_clients = num_clients + 1
+		#Limit number of clients server can handle to 5
+		if num_clients > 5:
+			c.send("Too many clients")
+			print "Fail: too many clients"
+			num_clients = num_clients - 1
+			print "Number of clients: " + str(num_clients)
+			return
+		#Otherwise prepare to receive data for new account
+		else:
+			c.send("Ready")
+			print "Number of clients: " + str(num_clients)
+		while True:
+			#Wait until username and password data is received
+			data = c.recv(1024)
+			#Encrypted data
+			print "Received:\nEncrypted message = "+str(data)
+			encrypted = eval(data)
+			#Decrypt message data
+			decrypted = private_key0.decrypt(encrypted)
+			 
+			#Confirm keyword in data received by account_maker
+			#Otherwise loop
+			if account_str in decrypted: 
+				#Remove extra characters
+				decrypted = decrypted.replace(account_str, '')
+				decrypted = decrypted.replace("\r\n", '')
+				#Send confirmation of reception (optional)
+				c.send("Server: OK")
+				print "Decrypted message = " + decrypted
+				#separate username and password
+				data = decrypted.split("~!@#$%^&*()")
+				#Insert into Mysql database
+				cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='poker')
+				cursor = cnx.cursor()
+				insert_stmt = ("INSERT INTO users (username, password) VALUES (%s, %s)")
+				cursor.execute(insert_stmt, (data[0], data[1], ))
+				print data[0]
+				print data[1]
+				#End Mysql connection
+				cnx.commit()
+				cursor.close()
+				cnx.close()
+			#End loop, end thread, and end connection
+			elif decrypted == "Quit": 
+				c.send("Thread closed\n")
+				#Update number of clients connected
+				num_clients = num_clients - 1
+				print "Number of clients: "+str(num_clients)
+				#if num_clients <= 0: break
+				break
+
+if __name__ == '__main__':
+	Main()
+# By Timothy Fong
+# CMPE 209 Poker Project
